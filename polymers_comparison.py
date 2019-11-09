6686a8dd94ebfb29d4fea814e7ff651c479c7a71
yaroslavsobolev <yaroslav.sobolev@phystech.edu>
2019-11-09 21:30:46 +0900

    Clean up some comments

--- polymers_comparison.py
+++ polymers_comparison.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-base_folder = 'D:/Docs/Science/UNIST/Projects/shear_crystal_growth/Data/Size distribution data for 18 crystals/'
 def get_sizes_from_file(target_folder, file):
     data = np.zeros(shape=(2,2))
     target_file = target_folder + file
@@ -14,110 +13,29 @@ def get_sizes_from_file(target_folder, file):
                break
     temp_data = np.genfromtxt(target_file, delimiter='\t', skip_header=8, max_rows=uptoline - 8)
     return temp_data
-    # data = temp_data
-    # if in_mm:
-    #     data[:,1] = data[:,1]*1000
-    # if um_to_mm:
-    #     data[:, 1] = data[:, 1] / 1000
-    # if no_vertlines:
-    #     lw = 0
-    # else:
-    #     lw = 0.6
-    # if axis:
-    #     axis.hist(data[:,1], alpha=alpha, density=True, edgecolor='grey',linewidth=lw, bins=thebins)
-    #     axis.hist(data[:,1], histtype='step', stacked=True, density=True,
-    #              fill=False, color = 'black', linewidth=1.5, bins=thebins)
-    # else:
-    #     plt.hist(data[:,1], alpha=alpha, density=True, bins=thebins)
-    #     plt.hist(data[:,1], histtype='step', stacked=True, density=True,
-    #              fill=False, color = 'black', linewidth=1.5, bins=thebins)
 
-# data = np.random.rand(2,3)
-# plt.boxplot(data)
-# plt.show()
-
-# function for setting the colors of the box plots pairs
-from pylab import plot, show, savefig, xlim, figure, \
-                hold, ylim, legend, boxplot, setp, axes
-
-# def setBoxColors(bp):
-    # setp(bp['boxes'][0], color='blue')
-    # setp(bp['caps'][0], color='blue')
-    # setp(bp['caps'][1], color='blue')
-    # setp(bp['whiskers'][0], color='blue')
-    # setp(bp['whiskers'][1], color='blue')
-    # plt.setp(bp['fliers'][0], markeredgecolor='blue')
-    # plt.setp(bp['fliers'][1], markeredgecolor='blue')
-    # setp(bp['medians'][0], color='blue')
-
-    # setp(bp['boxes'][1], color='red')
-    # setp(bp['caps'][2], color='red')
-    # setp(bp['caps'][3], color='red')
-    # setp(bp['whiskers'][2], color='red')
-    # setp(bp['whiskers'][3], color='red')
-    # plt.setp(bp['fliers'][1], markeredgecolor='red')
-    # plt.setp(bp['fliers'][3], markeredgecolor='blue')
-    # setp(bp['medians'][1], color='red')
-
-# Some fake data to plot
-
+from pylab import plot, show, savefig, xlim, figure, ylim, legend, boxplot, setp, axes
 N = 3
 data_stirring = []
-# data_nostirring = []
-# THIS IS FOR GROWTH FROM POWDER
-# data_stirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-#                                          'Data/Stirring speed dependent crystallization-with PIL/'
-#                                          '400 rpm/', 'size distribution-008.txt')[:,1])
-# data_nostirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-#                                          'Data/Growth in PIL without shear/', 'size distribution-008.txt')[:,1])
 
-data_stirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-                                         'Data/Data for Figure 2c/',
+data_stirring.append(get_sizes_from_file('data/crystal_growth/'
+                                         'Data for Figure 2c/',
                                          'TA without PIL-200 rpm- 3h.txt')[:,1])
-data_stirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-                                         'Data/Data for Figure 2c/',
+data_stirring.append(get_sizes_from_file('data/crystal_growth/'
+                                         'Data for Figure 2c/',
                                          'TA-75 mg PIL-0.75 ml DMF-200 rpm- 3h- Size distribution.txt')[:,1])
-data_stirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-                                         'Data/Data for Figure 2c/',
+data_stirring.append(get_sizes_from_file('data/crystal_growth/'
+                                         'Data for Figure 2c/',
                                          'TA-75 mg PMMA-0.75 ml DMF-200 rpm-3h.txt')[:,1])
-data_stirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-                                         'Data/Data for Figure 2c/',
+data_stirring.append(get_sizes_from_file('data/crystal_growth/'
+                                         'Data for Figure 2c/',
                                          'TA-75 mg PVDF-0.75 ml DMF-200 rpm-3h.txt')[:,1])
-# data_nostirring.append(get_sizes_from_file('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/'
-#                                          'Data/Time dependent growth from solution/No stirring -SD/',
-#                                          '10 min-SD.txt')[:,1])
 print(data_stirring)
-# print(data_nostirring)
-
-# filenames_list_stirring = ['SD-Crystal {0}-stirring.txt'.format(i+1) for i in range(N)]
-# for f in filenames_list_stirring:
-#     data_stirring.append(get_sizes_from_file(base_folder, f)[:,1])
-
-# filenames_list_nostirring = ['SD-Crystal {0}-non-stirring.txt'.format(i+1) for i in range(N)]
-# for f in filenames_list_nostirring:
-#     data_nostirring.append(get_sizes_from_file(base_folder, f)[:,1])
-
-# add data for trimesic acid
-
-# N = 3
-
-# data_stirring = np.random.rand(10,18)
-# data_nostirring = np.random.rand(10,18)
-# A= [[1, 2, 5,],  [7, 2]]
-# B = [[5, 7, 2, 2, 5], [7, 2, 5]]
-# C = [[3,2,5,7], [6, 7, 3]]
 
 fig = figure(figsize=(3,2.5))
 ax = axes()
-hold(True)
-
-# first boxplot pair
-# for i in range(N):
 bp = boxplot([data_stirring[0], data_stirring[1], data_stirring[2], data_stirring[3]],
              positions = [1.5+3*i for i in range(4)], widths = 0.6, whis=1000)
-# print('Number {0}, factor {1}'.format(i, np.mean(data_stirring[i])/np.mean(data_nostirring[i])))
-# print(np.mean(data_stirring[i])/np.mean(data_nostirring[i]))
-# setBoxColors(bp)
 alpha = 0.3
 for x in bp['boxes']:
     x.set(alpha=alpha)
@@ -135,37 +53,15 @@ for median in bp['medians']:
 for x in range(N):
     plt.axvline(x=3 + 3*x, color = 'black', alpha=0.15)
 
-
-# # second boxplot pair
-# bp = boxplot(B, positions = [4, 5], widths = 0.6)
-# setBoxColors(bp)
-#
-# # thrid boxplot pair
-# bp = boxplot(C, positions = [7, 8], widths = 0.6)
-# setBoxColors(bp)
-
-# set axes limits and labels
-# ylim(0,9)
 labels = ['No\npolymer', 'PIL-1', 'PMMA', 'PVDF']
 labels.extend([1+i for i in range(N)])
-# labels.append('TA')
 ax.set_xticklabels(labels)
 theticks = [1.5 + 3*i for i in range(N+1)]
-# theticks.append(20)
 ax.set_xticks(theticks)
 ax.set_yscale('log')
 ax.set_xlim(0,N*3+3)
-
-# draw temporary red and blue lines and use them to create a legend
-# hB, = plt.plot([1,1],'b-')
-# hR, = plt.plot([1,1],'r-')
-# legend((hR, hB),('With shear', 'Without shear'))
-# hB.set_visible(False)
-# hR.set_visible(False)
-# plt.xlabel('Substance index')
 plt.ylabel('Crystal size, µm')
 ax.xaxis.set_tick_params(length=0)
 plt.tight_layout()
-
-plt.savefig('polymer_compare.png', dpi=300)
+plt.savefig('figures/polymer_compare.png', dpi=300)
 plt.show()
--- simulation_plots.py
+++ simulation_plots.py
@@ -9,24 +9,21 @@ def plot_simulation_shear():
     data = np.array([[0.005, 340], [0.02, 643], [0.05, 1100], [0.1, 1585]])
     ax1.plot(data[:,0]*2, data[:,1]*fuckup_correction_coeff, color = 'blue', label='Freely rotating, $r_c$ = 0.2 $\mu$m')
 
-    data = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/rad_200nm.txt', skiprows=1)
+    data = np.loadtxt('data/simulation/rad_200nm.txt', skiprows=1)
     line1 = ax1.plot(data[:,0]*2*1000, data[:,2]*fuckup_correction_coeff, dashes=[4,4], color='blue', label='Fixed, $r_c$ = 0.2 $\mu$m')
 
     data = np.array([[0.25, 1460], [0.2, 1000], [0.17, 816], [0.15, 710], [0.1, 525], [0.05, 346.8], [0.01, 165]])
     ax1.plot(data[:,0]*2, data[:,1]*fuckup_correction_coeff, color = 'black', label='Freely rotating, $r_c$ = 2.0 $\mu$m')
 
-    data = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/rad_2um.txt', skiprows=1)
+    data = np.loadtxt('data/simulation/rad_2um.txt', skiprows=1)
     ax1.plot(data[:,0]*2*1000, data[:,2]*fuckup_correction_coeff, dashes=[4,4], color = 'black', label='Fixed, $r_c$ = 2.0 $\mu$m')
 
     data = np.array([[0, 41.868], [0.88, 41.868]])
     line1 = ax1.plot(data[:,0], data[:,1]*fuckup_correction_coeff, dashes=[1,1], color='green', label='Mean shear (bulk)')
     plt.xlim([0,0.5])
     plt.ylim([0, 6])
-    # ax1.get_xaxis().set_ticks([0, 0.1, .2, .3, .4, .5])
-    # ax1.get_yaxis().set_ticks([0, 200, 400, 600, 800, ])
     plt.ylabel('Local shear rate max., $10^3 $s$^{-1}$')
     plt.xlabel('Crystal size (edge length), mm')
-    # plt.legend(loc=(0.55, 0.4))
     plt.tight_layout()
     plt.savefig('theor_graph_1.png', dpi=300)
     plt.show()
@@ -37,24 +34,18 @@ def plot_simulation_shear_dimensionless():
     data = np.array([[0.005, 340], [0.02, 643], [0.05, 1100], [0.1, 1585]])
     ax1.plot(data[:,0]*2/1000/0.2, data[:,1], color = 'blue', label='Freely rotating, $r_c$ = 0.2 $\mu$m')
 
-    data = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/rad_200nm.txt', skiprows=1)
+    data = np.loadtxt('data/rad_200nm.txt', skiprows=1)
     line1 = ax1.plot(data[:,0]*2/0.2, data[:,2], dashes=[4,4], color='blue', label='Fixed, $r_c$ = 0.2 $\mu$m')
 
     data = np.array([[0.25, 1460], [0.2, 1000], [0.17, 816], [0.15, 710], [0.1, 525], [0.05, 346.8], [0.01, 165]])
     ax1.plot(data[:,0]*2/1000/2, data[:,1], color = 'black', label='Freely rotating, $r_c$ = 2.0 $\mu$m')
 
-    data = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/rad_2um.txt', skiprows=1)
+    data = np.loadtxt('data/simulation/rad_2um.txt', skiprows=1)
     ax1.plot(data[:,0]*2/2, data[:,2], dashes=[4,4], color = 'black', label='Fixed, $r_c$ = 2.0 $\mu$m')
 
     data = np.array([[0, 41.868], [0.88, 41.868]])
-    # line1 = ax1.plot(data[:,0], data[:,1], dashes=[1,1], color='green', label='Mean shear (bulk)')
-    # plt.xlim([0,0.5])
-    plt.ylim([0, 1600])
-    # ax1.get_xaxis().set_ticks([0, 0.1, .2, .3, .4, .5])
-    # ax1.get_yaxis().set_ticks([0, 200, 400, 600, 800, ])
     plt.ylabel('Local shear rate max., $10^3 s^{-1}$')
     plt.xlabel('$L/r_{c}$')
-    # plt.legend(loc=(0.55, 0.4))
     plt.tight_layout()
     plt.savefig('theor_graph_2.png', dpi=300)
     plt.show()
@@ -62,7 +53,7 @@ def plot_simulation_shear_dimensionless():
 def plot_relaxation():
     major_fuckup_correction = 1/42*167
     f1, ax1 = plt.subplots(1, sharex=True, sharey=False, figsize=(6, 4))
-    data = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/max_speed_vs_time.txt', skiprows=49)
+    data = np.loadtxt('data/simulation/max_speed_vs_time.txt', skiprows=49)
     ax1.plot((data[:,0]-data[0,0])*1000, data[:,1]/(0.1E-3*np.sqrt(2))*major_fuckup_correction, color = 'black', label='Simulation')
     plt.axhline(y=42/2*major_fuckup_correction, linestyle='--', label='$\dot{\gamma}_0/2$')
     print('V={0} mm/s'.format(41*0.1*np.sqrt(2)))
@@ -76,39 +67,24 @@ def plot_relaxation():
 
 
 f1, ax1 = plt.subplots(1, sharex=True, sharey=False, figsize=(8.9, 2.6))
-data = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/max_shear_vs_time_propergammazero.txt', skiprows=233)
+data = np.loadtxt('data/simulation/max_shear_vs_time_propergammazero.txt', skiprows=233)
 ax1.plot((data[:,0]-data[0,0])*1000, data[:,1], label='Newtonian', alpha=0.7)
-# plt.axhline(y=42/2, linestyle='--', label='$\dot{\gamma}_0/2$')
 print('V={0} mm/s'.format(41*0.1*np.sqrt(2)))
 plt.xlabel('t, ms')
 plt.ylabel('Highest shear rate, 1/s')
-# plt.yticks([0, 100, 200, 300, 400, 500, 600])
 plt.ylim(-0.1, 2800)
 plt.tight_layout()
-# plt.legend()
-# plt.savefig('theor_shear_rate_1.png')
-# plt.show()
-
-
-
-data2 = np.loadtxt('D:/Docs/Science/UNIST/Projects/shear_crystal_growth/simulation/max_shear_vs_time_nonnewtonian_1.txt', skiprows=1)
-
+data2 = np.loadtxt('data/simulation/max_shear_vs_time_nonnewtonian_1.txt', skiprows=1)
 dydx = np.gradient(data2[:,1], data2[:,0])
-# f2 = plt.figure(2)
-# plt.plot((data[:,0]-data[0,0])*1000, dydx)
-# f1, ax1 = plt.subplots(1, sharex=True, sharey=False, figsize=(8.9, 2.6))
-# ax1.plot((data[:,0]-data[0,0])*1000, data[:,1], color = 'black', label='Simulation2')
 thresh = 5e5
 todel = np.abs(dydx)<thresh
 y2 = data2[todel,1]
 x1 = (data2[:,0]-data2[0,0])*1000
 x2 = x1[todel]
 ax1.plot(x2[x2>10]+0.1, y2[x2>10], label='Non-newtonian', alpha=0.7)
-# plt.axhline(y=42/2, linestyle='--', label='$\dot{\gamma}_0/2$')
 print('V={0} mm/s'.format(41*0.1*np.sqrt(2)))
 plt.xlabel('t, ms')
 plt.ylabel('Highest shear rate, 1/s')
-# plt.yticks([0, 100, 200, 300, 400, 500, 600])
 plt.ylim(-0.1, 2800)
 plt.xlim(0, 45)
 plt.tight_layout()
